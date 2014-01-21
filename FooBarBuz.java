0dc046b007edd1a8f037656753f91f75d2f85227
Anusha Chenreddy <anusha.chenreddy@gmail.com>
2014-01-21 11:03:14 -0500

    Initial Commit for FooBarBuz.java

---
+++ FooBarBuz.java
@@ -0,0 +1,19 @@
+
+public class FooBarBuz {
+	public static void main (String args[]) {
+		
+		for(int i=1; i<=50; i++){
+			 if (i%3 ==0 ){
+				 System.out.println("Foo");
+			 }
+			 else if (i%5 == 0 ){
+				 System.out.println("Foo Bar");
+			 }
+			 else {
+				 System.out.println(i);
+			 }
+		 }
+		
+	}
+
+}
