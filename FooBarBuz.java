153e4ee473db6e20497a010c87f238dd129aab89
Anusha Chenreddy <anusha.chenreddy@gmail.com>
2014-01-21 22:01:54 -0500

    Modifying the program as per Task #3

--- FooBarBuz.java
+++ FooBarBuz.java
@@ -1,26 +1,63 @@
 
+import java.util.Scanner;
+import java.io.*;
 public class FooBarBuz {
-	public static void main (String args[]) {
-		
-		for(int i=1; i<=110; i++){
-			 
-          if (i%3 == 0 && i%5 == 0){
-				 System.out.println("Foo Bar");
-             }
-          else if (i%5 == 0 ){
-				 System.out.println("Bar");
-			 }
-			 else if (i%3 ==0 ){
-				 System.out.println("Foo");
-			 }
-			 else if (i%7 == 0 ){
-				 System.out.println("Baz");
-			 }
-			 else {
-				 System.out.println(i);
-			 }
-		 }
-		
+   static final String output_path = "C:\\Users\\Anusha\\Desktop\\Spring'14\\Studio\\CourseWork\\foobarbuz.txt";
+   static FileWriter fw;
+	public static void main (String args[])throws Exception {
+      fw = new FileWriter(output_path,false);	
+      while(true){
+      String out = foobar();
+         if (out.equals("0")){break;}
+      System.out.println(out);
+      writeFile(out);}
+      				
 	}
-
+  
+	// Method to write some text content on to a file.
+   public static void writeFile(String input){
+            try{
+         fw = new FileWriter(output_path,true);
+         fw.write(input + "\n");
+         fw.close();
+      }
+      catch(Exception e){
+         System.out.println(e.getMessage());
+      }
+   }
+   
+   // This method takes input at runtime and returns appropriate stmt.
+   // Takes any valid integer without any specific upper bound.
+   public static String foobar(){
+      Scanner scan = new Scanner(System.in);
+      try{		
+   		while(true){
+         System.out.println("Please enter an integer. Enter 0 to stop:");
+   		int num = scan.nextInt();
+         if (num == 0){
+                return "0";   }			
+   	   if (num%3 == 0 && num%5 == 0){
+   //				 System.out.println("Foo Bar"); 
+            return "Foo Bar"; }
+         else if (num%5 == 0 ){
+   //				 System.out.println("Bar");}
+            return "Bar";}
+   		else if (num%3 ==0 ){
+   //				 System.out.println("Foo"); }
+            return "Foo";}
+   		else if (num%7 == 0 ){
+   //				 System.out.println("Baz"); }
+            return "Baz";}
+   		else {
+   //				 System.out.println(num);
+            return num + "";
+             }
+         }
+		 }
+		catch(Exception e){
+//			System.out.println("Invalid Input");
+         return "Invalid Input";
+		}
+      
+   }
 }
