b10a19bd8b3b7e757a58e31bc7619857a4dda41b
Anusha Chenreddy <anusha.chenreddy@gmail.com>
2014-02-03 14:42:20 -0500

    created folder structure and wrote two classes for Issue and User

---
+++ githubapi-issues/src/main/java/Issue.java
@@ -0,0 +1,24 @@
+/**
+ * @author Anusha
+ *
+ */
+import java.util.*;
+
+public class Issue {
+
+	public Issue() {
+		super();
+	}
+
+		private long number;
+		private String id;
+		private String state;
+		private String title;
+		private String body;
+		private Date createdAt;
+		private Date closedAt;
+		private User user;
+		private User assignee;
+	
+
+}
