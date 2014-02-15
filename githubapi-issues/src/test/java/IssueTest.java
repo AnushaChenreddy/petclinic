50ef656f5e22de4cf2052a5a9b94a5ee98c40788
Anusha Chenreddy <anusha.chenreddy@gmail.com>
2014-02-15 12:33:14 -0500

    Added unit test testEquals() for Issue.equals() method

--- githubapi-issues/src/test/java/IssueTest.java
+++ githubapi-issues/src/test/java/IssueTest.java
@@ -9,13 +9,28 @@ import org.junit.Test;
 public class IssueTest {
 
 	@Test
-	public void testHashCode () {
+	public void testHashCode() {
 		Issue obj1 = new Issue();
 		obj1.setId("abccd");
 		Issue obj2 = new Issue();
-		obj2.setId("abccdf");
-//		User obj3 = new User();
-		assertEquals(obj1.hashCode(),obj2.hashCode());
+		obj2.setId("abccd");
+		assertEquals(obj1.hashCode(), obj2.hashCode());
+		assertEquals(obj1.hashCode(), obj1.hashCode());
+		assertTrue(obj1.hashCode() == obj2.hashCode());
+	}
+
+	@Test
+	public void testEquals() {
+		Issue obj1 = new Issue();
+		obj1.setId("6");
+		Issue obj2 = new Issue();
+		obj2.setId("6");
+		Issue obj3 = new Issue();
+		obj3.setId("abc");
+		assertFalse(obj1.equals(obj3));
+		assertTrue(obj1.equals(obj2));
+		assertEquals(obj1.equals(obj2), true);
+		assertEquals(obj1.equals(obj1), true);
 	}
 
 }
