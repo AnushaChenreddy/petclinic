f7e77b25859d2ba5f37f7dad11975457f4c0f450
Anusha Chenreddy <anusha.chenreddy@gmail.com>
2014-02-15 13:30:43 -0500

    unit test for toString() method of Issue class

--- githubapi-issues/src/test/java/IssueTest.java
+++ githubapi-issues/src/test/java/IssueTest.java
@@ -33,4 +33,49 @@ public class IssueTest {
 		assertEquals(obj1.equals(obj1), true);
 	}
 
+	@Test
+	/* Checking by passing a correct string */
+	public void testToString() {
+		Issue iss_obj = new Issue();
+		User user_obj = new User();
+		User assignee_obj = new User();
+		String tester = "Issue [number=1, " + "id=iss01, " + "state=true, "
+				+ "title=Title of Issue, " + "body=Body of Issue, "
+				+ "createdAt=null, " + "closedAt=null, "
+				+ "user=User [login=user1, id=userid01], "
+				+ "assignee=User [login=assignee1, id=userid01]]";
+		iss_obj.setId("iss01");
+		iss_obj.setNumber(1);
+		iss_obj.setState(true);
+		iss_obj.setTitle("Title of Issue");
+		iss_obj.setBody("Body of Issue");
+		user_obj.setId("userid01");
+		user_obj.setLogin("user1");
+		assignee_obj.setId("userid01");
+		assignee_obj.setLogin("assignee1");
+		iss_obj.setAssignee(assignee_obj);
+		iss_obj.setUser(user_obj);
+		assertEquals(tester, iss_obj.toString());
+
+		/* Passing a string with improper order */
+		Issue iss_obj1 = new Issue();
+		User user_obj1 = new User();
+		User assignee_obj1 = new User();
+		String test = "Issue [id=iss02, " + "number=2, " + "state=true, "
+				+ "title=null, " + "body=null, " + "createdAt=null, "
+				+ "closedAt=null, " + "user=User [id=userid02, login=user2], "
+				+ "assignee=User [id=userid02, login=assignee2]]";
+		iss_obj1.setId("iss02");
+		iss_obj1.setNumber(2);
+		iss_obj1.setState(true);
+		user_obj1.setId("userid02");
+		user_obj1.setLogin("user2");
+		assignee_obj1.setId("userid02");
+		assignee_obj1.setLogin("assignee2");
+		iss_obj1.setAssignee(assignee_obj1);
+		iss_obj1.setUser(user_obj1);
+		assertFalse(test == iss_obj1.toString());
+
+	}
+
 }
