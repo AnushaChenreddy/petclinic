545b6872229824f6b1fc81f9913dd4e510cfaff9
Anusha Chenreddy <anusha.chenreddy@gmail.com>
2014-02-16 17:32:02 -0500

    Created IssueParserTest Class and IssuParser Class

---
+++ githubapi-issues/src/main/java/IssueParser.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import java.util.ArrayList;
+
+public class IssueParser {
+	public ArrayList<Issue> parseIssues(String input){
+		ArrayList<Issue> issues = new ArrayList<Issue>();
+		return issues;
+	}
+}
